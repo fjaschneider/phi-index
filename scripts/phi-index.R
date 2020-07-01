65722e2dc6737d67ff40ff8ca6ed34ed35c18264
fjaschneider <33283311+fjaschneider@users.noreply.github.com>
2020-07-01 12:11:54 -0300

    Até o momento

--- scripts/phi-index.R
+++ scripts/phi-index.R
@@ -4,42 +4,41 @@ A_m2 <- 24384
 
 df <- read.csv('data/p_r.csv', sep = ';')
 summary(df)
+delta_t <- max(diff.Date(df$Time_min))
 
 DHR <- sum(c(0, (diff.Date(df$Time_min)*60))*(df$Q_m3s)/A_m2*1000)
 Rain <- sum(c(0, (diff.Date(df$Time_min)/60))*(df$R_mm_h))
-Dur <- sum(df$R_mm_h > 0)*(2)/60
+Dur <- sum(df$R_mm_h > 0)*(delta_t)/60
 I <- Rain - DHR
 phi_index <- I/Dur
 
-Dur2 <- sum(subset(df, R_mm_h > phi_index & R_mm_h != 0, select = R_mm_h) > 0)*(2)/60
-R_phi1 <- sum(subset(df, R_mm_h < phi_index & R_mm_h != 0, select = R_mm_h))*2/60
+Dur2 <- sum(subset(df, R_mm_h > phi_index & R_mm_h != 0, select = R_mm_h) > 0)*(delta_t)/60
+R_phi1 <- sum(subset(df, R_mm_h < phi_index & R_mm_h != 0, select = R_mm_h))*delta_t/60
 phi_index2 <- (Rain - DHR - R_phi1)/Dur2
 
 
-Dur3 <- sum(subset(df, R_mm_h > phi_index2 & R_mm_h != 0, select = R_mm_h) > 0)*(2)/60
-R_phi2 <- sum(subset(df, R_mm_h < phi_index2 & R_mm_h != 0, select = R_mm_h))*2/60
+Dur3 <- sum(subset(df, R_mm_h > phi_index2 & R_mm_h != 0, select = R_mm_h) > 0)*(delta_t)/60
+R_phi2 <- sum(subset(df, R_mm_h < phi_index2 & R_mm_h != 0, select = R_mm_h))*delta_t/60
 phi_index3 <- (Rain - DHR - R_phi2)/Dur3
 
 
-Dur4 <- sum(subset(df, R_mm_h > phi_index3 & R_mm_h != 0, select = R_mm_h) > 0)*(2)/60
-R_phi3 <- sum(subset(df, R_mm_h < phi_index3 & R_mm_h != 0, select = R_mm_h))*2/60
+Dur4 <- sum(subset(df, R_mm_h > phi_index3 & R_mm_h != 0, select = R_mm_h) > 0)*(delta_t)/60
+R_phi3 <- sum(subset(df, R_mm_h < phi_index3 & R_mm_h != 0, select = R_mm_h))*delta_t/60
 phi_index4 <- (Rain - DHR - R_phi3)/Dur4
 
 
-Dur5 <- sum(subset(df, R_mm_h > phi_index4 & R_mm_h != 0, select = R_mm_h) > 0)*(2)/60
-R_phi4 <- sum(subset(df, R_mm_h < phi_index4 & R_mm_h != 0, select = R_mm_h))*2/60
+Dur5 <- sum(subset(df, R_mm_h > phi_index4 & R_mm_h != 0, select = R_mm_h) > 0)*(delta_t)/60
+R_phi4 <- sum(subset(df, R_mm_h < phi_index4 & R_mm_h != 0, select = R_mm_h))*delta_t/60
 phi_index5 <- (Rain - DHR - R_phi4)/Dur5
 
 
-R_effective <- sum(subset(df, R_mm_h > phi_index5, select = R_mm_h) - phi_index5)*2/60
-time_effective <- sum(subset(df, R_mm_h > phi_index5, select = R_mm_h) - phi_index5 > 0)*2/60
+R_effective <- sum(subset(df, R_mm_h > phi_index5, select = R_mm_h) - phi_index5)*delta_t/60
+time_effective <- sum(subset(df, R_mm_h > phi_index5, select = R_mm_h) - phi_index5 > 0)*delta_t/60
 
 
-list('phi-index' = phi_index5,
-     'Rain effective' = R_effective,
-     'time effective' = time_effective)
+list('phi-index (mm/h)' = phi_index5,
+     'Rain effective (mm)' = R_effective,
+     'time effective (h)' = time_effective)
 
-function(data, rounds) {
-  
-  data <- subset(df, R_mm_h > 0, select = R_mm_h)
-}
+
+
